07e4a96e3d3ddd62f43bd44794aead18e97f10c0
Anton Kaiukov <batkowm@gmail.com>
2021-11-04 21:04:37 -0700

    Refactor mailer and add tests

--- mailer/mailer.go
+++ mailer/mailer.go
@@ -17,34 +17,60 @@ package mailer
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net/smtp"
 
 	"github.com/batk0/gc-tracker/config"
 )
 
+const subjectText = "Notification from GC-Tracker"
+const greetingText = "Hello!"
+const footerText = "This is automated message. Please do not reply."
+
+// Need for mocking
+type smtpMailer struct{}
+type mail interface {
+	SendMail(addr string, a smtp.Auth, from string, to []string, msg []byte) error
+}
+
+func (m *smtpMailer) SendMail(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
+	return smtp.SendMail(addr, a, from, to, msg)
+}
+
+// wraps sendReal to pass mailer interface
 func Send(to string, msg string) error {
+	var mailer smtpMailer = smtpMailer{}
+	return sendReal(&mailer, to, msg)
+}
+
+func getHeaders(from, to, subject string) string {
+	return fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\n", from, to, subject)
+}
+func getBody(msg string) string {
+	return fmt.Sprintf("%s\n\n%s\n\n%s\n", greetingText, msg, footerText)
+}
+
+func sendReal(mailer mail, to string, msg string) error {
+	if to == "" {
+		return errors.New("emtpty recepients list")
+	}
+	if msg == "" {
+		return errors.New("empty message")
+	}
 	toList := []string{to}
-	header := "From: " + config.Config.SMTPUser
-	header += "\nTo: " + to
-	header += `
-Subject: Notification from GC-Tracker
+	header := getHeaders(config.Config.SMTPUser, to, subjectText)
+	body := getBody(msg)
 
-Hello!
-
-`
-	footer := `
-This is automated message. Please do not reply.
-`
 	auth := smtp.PlainAuth("",
 		config.Config.SMTPUser,
 		config.Config.SMTPPass,
 		config.Config.SMTPHost)
-	if err := smtp.SendMail(config.Config.SMTPHost+":"+config.Config.SMTPPort,
+	if err := mailer.SendMail(config.Config.SMTPHost+":"+config.Config.SMTPPort,
 		auth,
 		config.Config.SMTPUser,
 		toList,
-		[]byte(header+msg+footer)); err != nil {
+		[]byte(header+body)); err != nil {
 		log.Println(err.Error())
 		return errors.New("cannot send email to " + to)
 	}
